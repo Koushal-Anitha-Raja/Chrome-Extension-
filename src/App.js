b6d7b89968f5047d1a74bb8b0497e595bf9e1738
Ar Koushal <arkoushal1999@gmail.com>
2023-09-07 14:47:13 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Choose your app List</h3>
+      <div>
+        <button>App A</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
